b708c7de099d122c75df224fdb43c76cc7528a61
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-09-01 11:44:22 -0500

    bearAndBigBrother solution

---
+++ bearAndBigBrother/bearAndBigBrother.js
@@ -0,0 +1,13 @@
+function bearAndBigBrother(a, b) {
+  let limak = a;
+  let bob = b;
+  let index = 0;
+  while (limak <= bob) {
+    limak *= 3;
+    bob *= 2;
+    index++;
+  }
+  return index;
+}
+
+bearAndBigBrother(4, 7);
