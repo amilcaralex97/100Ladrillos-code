88e6a6af6efc9adde78e765d77ef93ed3faa22c0
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-09-01 11:08:34 -0500

    diagonal bug fixesd

--- diagonalDifference/diagonalDifference.js
+++ diagonalDifference/diagonalDifference.js
@@ -7,30 +7,40 @@ const matrix = [
   [2, 8, 9],
 ];
 
+const matrix2 = [
+  [0, 15, 2, 1],
+  [8, 9, 8, 5],
+  [3, 3, 16, 7],
+  [12, 5, 22, 5],
+];
+
 function diagonalDifference(matrix) {
-  const length = matrix.length;
+  const length = matrix.length - 1;
   let leftDiagonal = 0;
   let rightDiagonal = 0;
-
-  for (let x = 0; x < length; x++) {
-    for (let y = 0; y < length; y++) {
+  let x = 0;
+  let y = 0;
+  let rightConditional = 0;
+  for (x = 0; x <= length; x++) {
+    for (y = 0; y <= length; y++) {
+      rightConditional = x + y;
       if (x === y) {
         leftDiagonal += matrix[x][y];
-        //console.log(x);
-      } else if (x + y === length - 1) {
-        console.log(true);
+      }
+      if (rightConditional === length) {
+        rightDiagonal += matrix[x][y];
         //rightDiagonal += matrix[x][y];
         //console.log(x + y, length - 1);
         //console.log(matrix[x][y]);
         //console.log(x, " ", y);
       }
-      console.log(x, " ", y);
     }
   }
-  //console.log(leftDiagonal);
-  //console.log(rightDiagonal);
+  console.log(leftDiagonal);
+  console.log(rightDiagonal);
+  return Math.abs(leftDiagonal - rightDiagonal);
 }
 
-diagonalDifference(matrix);
+console.log(diagonalDifference(matrix2));
 
 module.exports = diagonalDifference;
